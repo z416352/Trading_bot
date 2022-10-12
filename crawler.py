b429007d56bae6037b3221a3db94ccda5cf070a5
z416352 <43094601+z416352@users.noreply.github.com>
2022-10-13 01:23:51 +0800

    新增自動更新DB與建立DB程式

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 from functions import *
+import pandas as pd
 
 c, conn = init_db("test.db")
 
@@ -7,20 +8,9 @@ coin_txt = open("coin.txt","r",encoding = 'utf8')
 coin = coin_txt.read()
 coin_txt.close()
 
-create_table(c, coin)
+# create_table(c, coin)
 
-start_time = datetime(2022, 10, 4, 8, 0)
-end_time = datetime(2022, 10, 6, 8, 0)
+update_table(conn, coin)
 
-crypto_df = get_candle_data(start=start_time, end=end_time)
-# for i in range(len(crypto_price_data_list)):
-#     open_price  = crypto_price_data_list[i]["open"]
-#     high_price  = crypto_price_data_list[i]["high"]
-#     low_price   = crypto_price_data_list[i]["low"]
-#     close_price = crypto_price_data_list[i]["close"]
-#     volume      = crypto_price_data_list[i]["volume"]
-#     date_time   = str(millisecond2date(crypto_price_data_list[i]["period"]))
-#     # print("open:"+open_price+" high:"+high_price+" low:"+low_price+" close:"+close_price+" volume:"+volume+" date:"+date_time)
-#     insert_data(c, coin, open_price, high_price, low_price, close_price, volume, date_time)
-
-# conn.commit()
+df = show_all_data(conn, coin)
+print(df)
--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import sqlite3
 import requests
 import pandas as pd
@@ -12,7 +12,6 @@ def millisecond2date(milliseconds):
 
 def date2millisecond(date_time):
     return (int)(time.mktime(date_time.timetuple()) * 1000)
-
 
 def get_candle_data(exchange = "binance", interval = "d1", baseId = "bitcoin", quoteId = "tether", start = datetime.now(), end = datetime.now()):
     ## interval = m1, m5, m15, m30, h1, h2, h6, h12, d1
@@ -60,21 +59,59 @@ def init_db(db_name):
     return cursor, conn
 
 # 建立table
+# def create_table(cursor, coin_name):
+#     command = '''CREATE TABLE if not exists '{}' (
+# 	"cid"	 INTEGER NOT NULL,
+# 	"open"	 INTEGER,
+# 	"high"	 INTEGER,
+# 	"low"	 INTEGER,
+# 	"close"	 INTEGER,
+# 	"volume" INTEGER,
+#     "date"   TEXT ,
+# 	PRIMARY KEY("cid" AUTOINCREMENT)
+#     );'''.format(coin_name)
+
+#     cursor.execute(command)
+
+# # 新增資料
+# def insert_data(cursor, coin_table, open_price, high_price, low_price, close_price, volume, date_time):
+#     command = "insert into '{}'(open, high, low, close, volume, date) values('{}', '{}', '{}', '{}', '{}', '{}');".format(coin_table, open_price, high_price, low_price, close_price, volume, date_time)
+#     cursor.execute(command)
+
+
+# 建立table by chih
 def create_table(cursor, coin_name):
     command = '''CREATE TABLE if not exists '{}' (
-	"cid"	 INTEGER NOT NULL,
-	"open"	 INTEGER,
-	"high"	 INTEGER,
-	"low"	 INTEGER,
-	"close"	 INTEGER,
-	"volume" INTEGER,
-    "date"   TEXT ,
-	PRIMARY KEY("cid" AUTOINCREMENT)
+    "Timestamp"	 TEXT NOT NULL,
+    "Open"	 INTEGER,
+    "High"	 INTEGER,
+    "Low"	 INTEGER,
+    "Close"	 INTEGER,
+    "Volume" INTEGER,
+    PRIMARY KEY("Timestamp")
     );'''.format(coin_name)
-
+    
     cursor.execute(command)
 
-# 新增資料
-def insert_data(cursor, coin_table, open_price, high_price, low_price, close_price, volume, date_time):
-    command = "insert into '{}'(open, high, low, close, volume, date) values('{}', '{}', '{}', '{}', '{}', '{}');".format(coin_table, open_price, high_price, low_price, close_price, volume, date_time)
-    cursor.execute(command)
+
+def update_table(conn, coin_name):
+    last_data_df = pd.read_sql("SELECT * FROM "+ coin_name +" WHERE Timestamp=(SELECT MAX(Timestamp) FROM " + coin_name +")", conn)
+
+    last_date =  datetime.strptime(last_data_df['Timestamp'][0], '%Y-%m-%d %H:%M:%S')
+    start = last_date + timedelta(seconds=3) # 避免重複資料的問題
+    end = datetime.now()
+    crypto_df = get_candle_data(start=start, end=end)
+
+    try :
+        crypto_df.to_sql(coin_name, conn, if_exists='append', index=True, index_label='Timestamp') 
+    except sqlite3.IntegrityError:
+        print("ERROR: 可能有重複的資料")
+    except Exception:
+        print ("ERROR: 其他錯誤")
+    else:
+        print("新增完成")
+
+
+def show_all_data(conn, coin_name):
+    df = pd.read_sql("SELECT * FROM "+ coin_name, conn)
+    return df
