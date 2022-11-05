950d6f30693ab8b8e0f1dff741075cfe9c475deb
z416352 <43094601+z416352@users.noreply.github.com>
2022-11-06 02:25:23 +0800

    crawler debug

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -4,16 +4,15 @@ import pandas as pd
 # get from https://www.finlab.tw/btc-crawler-py/
 from crypto_backtrader.finlab import crypto
 
-
 # coin_txt 放入要存入的幣別
-coin_txt = open("coin.txt","r",encoding = 'utf8')
+coin_txt = open("../DATA/coin.txt","r",encoding = 'utf8')
 coin = coin_txt.read()
 coin_txt.close()
 
-DB = DB_table(db_name = "test.db", coin_name = coin)
+DB = DB_table(db_file_path="../DATA/test.db", coin_name=coin)
 
 DB.init_db()
+DB.create()
+DB.update(Interval='8h')
 
-DB.update(interval='h1')
-
-# DB.show()
+print(DB.get_all())
